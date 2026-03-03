d454e453a8286e747bf7a72b4e8055312e8cf1be
David Wanderley <davidgomeswanderley@gmail.com>
2026-03-03 19:16:12 -0300

    adicao do https

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,7 +4,7 @@ const allowedOrigins = [
   'http://localhost:5173',   
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'juri-modelos-app.vercel.app', 
+  'https://juri-modelos-app.vercel.app', 
 ];
 
 const corsOptions = {
