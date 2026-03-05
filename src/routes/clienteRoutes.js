62bc8f6100b260d96972e64995d3ee707230a991
David Wanderley <davidgomeswanderley@gmail.com>
2026-03-05 16:46:53 -0300

    feat: adicionar CRUD completo de clientes e endpoint de estatísticas

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -4,6 +4,9 @@ const clienteController = require('../controllers/clienteController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
 router.get('/', authMiddleware, clienteController.listarClientes);
+router.get('/:id', authMiddleware, clienteController.buscarCliente);
 router.post('/', authMiddleware, clienteController.criarCliente);
+router.put('/:id', authMiddleware, clienteController.atualizarCliente);
+router.delete('/:id', authMiddleware, clienteController.deletarCliente);
 
 module.exports = router;
